7283a2016c8683a7cf4cf7cea0f7af8bdda06bbe
NileKnight0 <ammartube777@gmail.com>
2026-09-07 15:21:32 +0300

    added simple parokour on room 1, now designing room 2

--- scripts/game.gd
+++ scripts/game.gd
@@ -7,11 +7,33 @@ func init_game():
 func init_lights():
 	$room_1/map_behind/lights.visible = 1
 	$player/flash.visible = 1
+	$hallway/lights.visible = 1
 	
 func _ready() -> void:
 	init_game()
 
+var main_room_door_1 = 0
+var main_room_door_2 = 0
+
+
+func _on_main_room_door_1_body_entered(body: Node2D) -> void:
+	if body == $player:
+		main_room_door_1 = 1
+func _on_main_room_door_1_body_exited(body: Node2D) -> void:
+	if body == $player:
+		main_room_door_1 = 0
+func _on_main_room_door_2_body_entered(body: Node2D) -> void:
+	if body == $player:
+		main_room_door_2 = 1
+func _on_main_room_door_2_body_exited(body: Node2D) -> void:
+	if body == $player:
+		main_room_door_2 = 0
 
 
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("interact"):
+		if main_room_door_1:
+			$player.position = Vector2(-177.0, 74)
+		elif main_room_door_2:
+			$player.position = Vector2(-169.0, 821.0)
+		
--- scripts/temp_player.gd
+++ scripts/temp_player.gd
@@ -30,15 +30,15 @@ func _physics_process(delta: float) -> void:
 		
 	if Input.is_action_pressed("sprint"):
 		SPEED = 600
-		JUMP_VELOCITY = -350.0
+		JUMP_VELOCITY = -400.0
 		sprint = 1
 	else:
 		sprint = 0
-		SPEED = 300
-		JUMP_VELOCITY = -250.0
+		SPEED = 300 
+		JUMP_VELOCITY = -350.0
 		
-	#if Input.is_action_just_pressed("jump") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 	var direction := Input.get_axis("left", "right")
 	if direction:
